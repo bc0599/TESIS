daebcc7d8980ea9fd5aaadf49ea1ba8dcc061094
bc0599 <betsabecohen123@gmail.com>
2020-03-09 14:10:17 -0400

    09/03/2020

--- src/app/pia-items/pia-items.module.ts
+++ src/app/pia-items/pia-items.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonSlides} from '@ionic/angular';
 
 import { IonicModule } from '@ionic/angular';
 
